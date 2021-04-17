fe8805dfcdff2a5466f0ff9d8ca7b78ff12dc5a6
Bravo <gmb2388@yahoo.com>
2021-04-16 22:59:25 -0700

    handled errors, redirect to homepage if no repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,11 +3,20 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function () {
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-    console.log(repoName);
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
 };
 
 var getRepoIssues = function (repo) {
@@ -29,8 +38,8 @@ var getRepoIssues = function (repo) {
             });
         }
         else {
-            console.log(response);
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -91,4 +100,3 @@ var displayWarning = function (repo) {
 };
 
 getRepoName();
-getRepoIssues();
