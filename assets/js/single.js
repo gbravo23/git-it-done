d841d9b5f09f6372621bb76b777d0a22498e5ea0
Bravo <gmb2388@yahoo.com>
2021-04-16 22:14:09 -0700

    get repo name

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,11 +2,19 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    console.log(repoName);
+};
+
 var getRepoIssues = function (repo) {
     // format the github api url
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-    // make a get request to url
+    // make a get request to url - double check!!!
     fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
@@ -82,4 +90,5 @@ var displayWarning = function (repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
+getRepoIssues();
